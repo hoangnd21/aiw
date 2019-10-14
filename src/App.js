cf61fdbaefef55a13e6df60274960efb218c208a
hoangnd21 <hoangnd21@gmail.com>
2019-10-14 12:16:26 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import {
   Card,
   Layout,
-  Row,
-  Col
 } from 'antd'
 const publicIp = require('public-ip');
 const AccuKey = 'CsSXU2U6I6o5S1zK7HVIHsSOw5ycY5ap';
-
 class App extends React.Component {
 
   state = {
     IP: null,
+    hanoiKey: '',
     location: {},
     currentCondition: {},
-    currentTemp: {},
+    currentTempC: {},
+    currentTempF: {},
     currentTime: '',
     oneDay: {},
     oneDayDay: {},
-    oneDayNight: {}
+    oneDayNight: {},
+    oneDayTempMin: {},
+    oneDayTempMax: {},
   }
-  // 353412 = Hanoi
   componentDidMount = () => {
     (async () => {
       const myIP = await publicIp.v4();
@@ -33,72 +33,82 @@ class App extends React.Component {
         .then(res => res.json())
         .then(data => {
           this.setState({
-            location: data
+            location: data,
+            hanoiKey: data.Key
           })
-          fetch(`http://dataservice.accuweather.com/currentconditions/v1/${data.Key}?apikey=${AccuKey}`)
+          const { hanoiKey } = this.state;
+          fetch(`http://dataservice.accuweather.com/currentconditions/v1/${hanoiKey}?apikey=${AccuKey}`)
             .then(res => res.json())
             .then(current => {
               this.setState({
                 currentCondition: current[0],
-                currentTemp: current[0].Temperature.Metric,
+                currentTempC: current[0].Temperature.Metric,
+                currentTempF: current[0].Temperature.Imperial,
                 currentTime: current[0].LocalObservationDateTime
               })
             })
-          fetch(`http://dataservice.accuweather.com//forecasts/v1/daily/1day/${data.Key}?apikey=${AccuKey}`)
+          fetch(`http://dataservice.accuweather.com//forecasts/v1/daily/1day/${hanoiKey}?apikey=${AccuKey}`)
             .then(res => res.json())
             .then(oneDay => {
               this.setState({
                 oneDay: oneDay.DailyForecasts[0],
                 oneDayDay: oneDay.DailyForecasts[0].Day,
-                oneDayNight: oneDay.DailyForecasts[0].Night
+                oneDayNight: oneDay.DailyForecasts[0].Night,
+                oneDayTempMin: oneDay.DailyForecasts[0].Temperature.Minimum,
+                oneDayTempMax: oneDay.DailyForecasts[0].Temperature.Maximum
               })
-              console.log(this.state.oneDay)
             })
         })
-
     })();
   }
 
   render() {
-    const { IP, location, currentCondition, currentTemp, currentTime, oneDay, oneDayDay, oneDayNight } = this.state;
-    console.log(oneDayDay, oneDayNight)
-    console.log(currentCondition)
+    const {
+      IP,
+      hanoiKey,
+      location,
+      currentCondition,
+      currentTempC,
+      currentTempF,
+      currentTime,
+      oneDay,
+      oneDayDay,
+      oneDayNight,
+      oneDayTempMin,
+      oneDayTempMax
+    } = this.state;
     return (
       <Layout className='layout'>
-        <h1>AIW</h1>
-        <Card className='card'>
-          My IP: {IP} <br />
-          My Location Key: {location.Key}<br />
-          My city: {location.EnglishName} <br />
+        <div style={{ fontSize: 45 }}>AIW</div >
+        <Card className='card' title={location ? <div style={{ color: 'green' }}>Get location success!</div> : null}>
+          <div style={{ marginLeft: '1em' }}>
+            <p>My IP: {IP}</p>
+            <p>My Location Key: {hanoiKey}</p>
+            <p>My city: {location.EnglishName}</p>
+          </div>
         </Card>
         <Card className='card' title={`Current weather in ${location.EnglishName}:`}>
-          <p>Current condition: {currentCondition.WeatherText}</p>
-          <p>Temperature: {currentTemp.Value}&#176;{currentTemp.Unit}</p>
-          <p >Weather is updated at:&nbsp;
+          <div style={{ marginLeft: '1em' }}>
+            <p >Weather is updated at:&nbsp;
             <span style={{ color: 'green' }}>
-              {currentTime.slice(11, 19)}
-              {currentCondition.IsDayTime === true ? 'AM' : 'PM'}&nbsp;
-              on&nbsp;
-              {currentTime.slice(1, 10)}
-            </span>
-          </p>
-          <p>Sources:&nbsp;<a href={currentCondition.Link}>AccuWeather</a></p>
+                {currentTime.slice(11, 19)}&nbsp;
+                {currentCondition.IsDayTime === true ? 'AM' : 'PM'}&nbsp;
+                on&nbsp;
+              {currentTime.slice(8, 10)}-{currentTime.slice(5, 7)}-{currentTime.slice(0, 4)}
+              </span>
+            </p>
+            <p>Current condition: {currentCondition.WeatherText}</p>
+            <p>Temperature: {currentTempF.Value}&#176;{currentTempF.Unit} ({currentTempC.Value}&#176;{currentTempC.Unit})</p>
+          </div>
         </Card>
-        <Card className='card' title={`Today's forecast in ${location.EnglishName}`}>
-          <Row gutter={16}>
-            <Col xl={12} className='card'>
-              <Card>
-                {oneDayDay.IconPhrase}
-              </Card>
-            </Col>
-            <Col xl={12} className='card'>
-              <Card>
-                {oneDayNight.IconPhrase}
-              </Card>
-            </Col>
-          </Row>
-          <div>Sources:&nbsp;<a href={oneDay.Link}>{oneDay.Sources}</a></div>
+        <Card className='card' title={`Today's forecast in ${location.EnglishName}:`}>
+          <div style={{ marginLeft: '1em' }}>
+            <p>Temperature: {oneDayTempMin.Value}&#176;{oneDayTempMin.Unit} to {oneDayTempMax.Value}&#176;{oneDayTempMax.Unit}<br /></p>
+            <p>Day: {oneDayDay.IconPhrase}</p>
+            <p>Night: {oneDayNight.IconPhrase}</p>
+          </div>
         </Card>
+        <p>Sources:&nbsp;<a href={oneDay.Link}>{oneDay.Sources}</a></p>
       </Layout>
     );
   }
